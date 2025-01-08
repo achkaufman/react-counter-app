f93679a6b8a2bf5d88a3ddc55bae9ca34b1968b4
Amanda Kaufman <47983139+achkaufman@users.noreply.github.com>
2025-01-08 09:17:42 -0700

    Refactor code to extract increment/decrement onClick functions

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,18 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 
 function Counter() {
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0)
     useEffect(() => {
         console.log('Component has rendered')
         return () => {
             console.log('Cleanup before next render or unmount')
         }
     }, [count])
+    const handleClickIncrement = () => {
+        setCount(count + 1)
+    }
+    const handleClickDecrement = () => {
+        setCount(count - 1)
+    }
     return (
         <div>
             <h1>Count: {count}</h1>
-            <button className="counter-button" onClick={() => setCount(count + 1)}>Increment</button>
-            <button className="counter-button" onClick={() => setCount(count - 1)}>Decrement</button>
+            <button className="counter-button" onClick={handleClickIncrement}>Increment</button>
+            <button className="counter-button" onClick={handleClickDecrement}>Decrement</button>
             {(count > 0) && <p>Count is a positive number.</p>}
             {(count === 0) && <p>Count is not positive or negative.</p>}
             {(count < 0) && <p>Count is a negative number.</p>}
