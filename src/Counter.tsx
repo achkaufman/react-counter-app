74da28e414353492ac1732f732465722e9e30017
Amanda Kaufman <47983139+achkaufman@users.noreply.github.com>
2025-01-08 09:21:16 -0700

    Add handleMouseOver functions for increment/decrement to learn event handling

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -14,11 +14,23 @@ function Counter() {
     const handleClickDecrement = () => {
         setCount(count - 1)
     }
+    const handleMouseOverIncrement = () => {
+        console.log('Mouse over Increment button!')
+    }
+    const handleMouseOverDecrement = () => {
+        console.log('Mouse over Decrement button!')
+    }
     return (
         <div>
             <h1>Count: {count}</h1>
-            <button className="counter-button" onClick={handleClickIncrement}>Increment</button>
-            <button className="counter-button" onClick={handleClickDecrement}>Decrement</button>
+            <button
+                className="counter-button"
+                onClick={handleClickIncrement}
+                onMouseOver={handleMouseOverIncrement}>Increment</button>
+            <button
+                className="counter-button"
+                onClick={handleClickDecrement}
+                onMouseOver={handleMouseOverDecrement}>Decrement</button>
             {(count > 0) && <p>Count is a positive number.</p>}
             {(count === 0) && <p>Count is not positive or negative.</p>}
             {(count < 0) && <p>Count is a negative number.</p>}
