ea989e79b50d9e725b25d100447e13112cc0c666
Amanda Kaufman <aka@microsoft.com>
2025-01-08 08:31:45 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# React Counter App
+
+A basic counter with increment and decrement functionality, built on a starter React project created with npx.
+
+First run `npm install` in the root directory to install dependencies, then `npm start` to start the app.
---
+++ src/Counter.tsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+
+function Counter() {
+    const [count, setCount] = useState(0);
+    useEffect(() => {
+        console.log('Component has rendered')
+        return () => {
+            console.log('Cleanup before next render or unmount')
+        }
+    }, [count])
+    return (
+        <div>
+            <h1>Count: {count}</h1>
+            <button className="counter-button" onClick={() => setCount(count + 1)}>Increment</button>
+            <button className="counter-button" onClick={() => setCount(count - 1)}>Decrement</button>
+            {(count >= 0) ? <p>Count is a positive number.</p> : <p>Count is a negative number.</p>}
+        </div>
+    )
+}
+
+export default Counter;
