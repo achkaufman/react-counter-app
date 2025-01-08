2bf7b3f74f33d276b96ab76eb0ff1b85b9bad900
Amanda Kaufman <47983139+achkaufman@users.noreply.github.com>
2025-01-08 08:53:45 -0700

    Fix conditional rendering statements to be more accurate

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -13,7 +13,9 @@ function Counter() {
             <h1>Count: {count}</h1>
             <button className="counter-button" onClick={() => setCount(count + 1)}>Increment</button>
             <button className="counter-button" onClick={() => setCount(count - 1)}>Decrement</button>
-            {(count >= 0) ? <p>Count is a positive number.</p> : <p>Count is a negative number.</p>}
+            {(count > 0) && <p>Count is a positive number.</p>}
+            {(count === 0) && <p>Count is not positive or negative.</p>}
+            {(count < 0) && <p>Count is a negative number.</p>}
         </div>
     )
 }
